c59c32073dc354b768cd278cfa1fa508a088b31f
tayyipgoren <tayyipgoren@gmail.com>
2017-11-26 16:20:42 +0300

    First release
    Signed-off-by: tayyipgoren <tayyipgoren@gmail.com>

--- src/GreedyHeuristic/GreedyH.java
+++ src/GreedyHeuristic/GreedyH.java
@@ -21,7 +21,7 @@ public class GreedyH {
         System.out.println("");
         sc.nextLine();
         //Getting file name from User
-        System.out.print("Please input file name eg.(gsp_turkiye.txt) :");
+        System.out.print("Please input file name eg.(gsp_turkiye.txt)or(gsp_dunya.txt) :");
         String fileName = "src/"+sc.nextLine();
         System.out.println("");
         long startTime = System.currentTimeMillis();
@@ -31,23 +31,23 @@ public class GreedyH {
         //Finding the route
         PrintWriter  pt = null;
         try {
-          pt = new PrintWriter(fileName.replaceAll(".txt","")+"_for"+N+"_sonuc.txt","UTF-8");
+            pt = new PrintWriter(fileName.replaceAll(".txt","")+"_for"+N+"_sonuc.txt","UTF-8");
         } catch (Exception e) {
             System.err.println("Error :"+e);
         }
-        System.out.println(findNearest(cities,0,0,pt));
+        findNearest(cities,0,0,0,pt);
         long endTime = System.currentTimeMillis();
         pt.println("Number of cities : "+N);
-        pt.println("Total Runnig Time :"+(endTime-startTime)+"ms");
+        pt.println("Total Running Time :"+(endTime-startTime)+"ms");
         
         pt.close();
-        
+
     }
-    
-    public static String findNearest(City[] cities,int index,double total,PrintWriter pt)
+
+    public static String findNearest(City[] cities,int index,int dep,double total,PrintWriter pt)
     {
         if(cities[index].flag == false)
-            return findNearestBack(cities,index,total,pt);
+            return findNearestBack(cities,index,dep,total,pt);
         double[] min = new double[2];
         min[0] = Double.MAX_VALUE;
         cities[index].flag = false;
@@ -70,14 +70,14 @@ public class GreedyH {
         {
             System.out.println("\nTotal Calculation :"+cities[(int)min[1]].name+"----- "+(String.format("%.2f",(total * 0.001)))+"km "+"------>"+cities[index].name+"\n");
             pt.println("\nTotal Calculation :"+cities[(int)min[1]].name+"----- "+(String.format("%.2f",(total * 0.001)))+"km "+"------>"+cities[index].name+"\n");
-            return findNearest(cities,index,total,pt);
+            return findNearest(cities,index,dep,total,pt);
         }
         System.out.println(cities[index].name+"["+cities[index].country.trim()+"]"+"---"+(String.format("%.2f",(min[0] * 0.001)))+"km"+"--->"+cities[(int)min[1]].name+"["+cities[(int)min[1]].country.trim()+"]");
         pt.println(cities[index].name+"["+cities[index].country.trim()+"]"+"---"+(String.format("%.2f",(min[0] * 0.001)))+"km"+"--->"+cities[(int)min[1]].name+"["+cities[(int)min[1]].country.trim()+"]");
-        return findNearest(cities,(int)min[1],total += min[0],pt);
+        return findNearest(cities,(int)min[1],dep,total += min[0],pt);
     }
     
-    public static String findNearestBack(City[] cities,int index,double total,PrintWriter pt)
+    public static String findNearestBack(City[] cities,int index,int dep,double total,PrintWriter pt)
     {
         if(cities[index].flag == true)
             return (String.format("%.2f",(total * 0.001)));
@@ -103,11 +103,20 @@ public class GreedyH {
         {
             System.out.println("\nTotal Calculation :"+cities[(int)min[1]].name+"----- "+(String.format("%.2f",(total * 0.001)))+"km "+"------>"+cities[index].name);
             pt.println("\nTotal Calculation Back:"+cities[(int)min[1]].name+"----- "+(String.format("%.2f",(total * 0.001)))+"km "+"------>"+cities[index].name);
-            return findNearestBack(cities,(int)min[1],total,pt);
+            return findNearestBack(cities,(int)min[1],dep,total,pt);
+        }
+        if ((int)min[1] == dep)
+        {
+             System.out.println(cities[index].name+"["+cities[index].country.trim()+"]"+"---"+(String.format("%.2f",(min[0] * 0.001)))+"km"+"--->"+cities[(int)min[1]].name+"["+cities[(int)min[1]].country.trim()+"]");
+            pt.println(cities[index].name+"["+cities[index].country.trim()+"]"+"---"+(String.format("%.2f",(min[0] * 0.001)))+"km"+"--->"+cities[(int)min[1]].name+"["+cities[(int)min[1]].country.trim()+"]");
+            
+            System.out.println("\nTotal Calculation Back:"+cities[dep].name+"----- "+(String.format("%.2f",(total * 0.001)))+"km "+"------>"+cities[dep].name);
+            pt.println("\nTotal Calculation Back:"+cities[dep].name+"----- "+(String.format("%.2f",(total * 0.001)))+"km "+"------>"+cities[dep].name);
+            return (String.format("%.2f",(total * 0.001)));
         }
         System.out.println(cities[index].name+"["+cities[index].country.trim()+"]"+"---"+(String.format("%.2f",(min[0] * 0.001)))+"km"+"--->"+cities[(int)min[1]].name+"["+cities[(int)min[1]].country.trim()+"]");
         pt.println(cities[index].name+"["+cities[index].country.trim()+"]"+"---"+(String.format("%.2f",(min[0] * 0.001)))+"km"+"--->"+cities[(int)min[1]].name+"["+cities[(int)min[1]].country.trim()+"]");
-        return findNearestBack(cities,(int)min[1],total += min[0],pt);
+        return findNearestBack(cities,(int)min[1],dep,total += min[0],pt);
     }
     
     public static boolean readFile(City[] cities,String fileName)
